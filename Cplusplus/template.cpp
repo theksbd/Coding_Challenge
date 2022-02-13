c40631d19f2fb1caaa49c00bca05651d3eb2d04f
Do Thien Hoang <theksbd@gmail.com>
2022-02-13 13:27:46 +0700

    Add template.cpp

---
+++ Cplusplus/template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <iomanip>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    // Solution v1.0 - Time complexity: O() ~ ms - Memory: mb
+    return 0;
+}
