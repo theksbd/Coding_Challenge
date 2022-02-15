fe819f8d7b5249766c64121387526ee123e82758
Do Thien Hoang <theksbd@gmail.com>
2022-02-15 11:04:50 +0700

    Add another solution(s) for this problem

--- C++/Leetcode/SingleNumber.cpp
+++ C++/Leetcode/SingleNumber.cpp
@@ -16,20 +16,31 @@ using namespace std;
 class Solution
 {
 public:
+    // Solution v1.0 - Time complexity: O(nlogn) ~ 28ms - Memory: 16.8mb
+    // int singleNumber(vector<int> &nums)
+    // {
+    //     if (nums.size() == 1)
+    //         return nums[0];
+    //     sort(nums.begin(), nums.end());
+    //     int size = nums.size();
+    //     if (nums[size - 1] != nums[size - 2])
+    //         return nums[size - 1];
+    //     for (int i = 0; i < size; i += 2)
+    //     {
+    //         if (nums[i] != nums[i + 1])
+    //             return nums[i];
+    //     }
+    //     return 0;
+    // }
+
+    // Solution v2.0 - Time complexity: O(n) ~ 16ms - Memory: 17mb
     int singleNumber(vector<int> &nums)
     {
-        if (nums.size() == 1)
-            return nums[0];
-        sort(nums.begin(), nums.end());
+        int res = nums[0];
         int size = nums.size();
-        if (nums[size - 1] != nums[size - 2])
-            return nums[size - 1];
-        for (int i = 0; i < size; i += 2)
-        {
-            if (nums[i] != nums[i + 1])
-                return nums[i];
-        }
-        return 0;
+        for (int i = 1; i < size; i++)
+            res ^= nums[i];
+        return res;
     }
 };
 
