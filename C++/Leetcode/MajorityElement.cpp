c05d940d89405b73e5c3e244a29f423e34a6603c
Do Thien Hoang <theksbd@gmail.com>
2022-02-21 12:13:27 +0700

    Update changes

--- C++/Leetcode/MajorityElement.cpp
+++ C++/Leetcode/MajorityElement.cpp
@@ -19,19 +19,42 @@ using namespace std;
 class Solution
 {
 public:
+    // Solution v1.0 - Time complexity: O(n) ~ 24ms - Memory: 19.6mb
+    // int majorityElement(vector<int> &nums)
+    // {
+    //     if (nums.size() == 1)
+    //         return nums[0];
+    //     map<int, int> freq;
+    //     int count = nums.size() / 2;
+    //     for (int x : nums)
+    //     {
+    //         freq[x]++;
+    //         if (freq[x] > count)
+    //             return x;
+    //     }
+    //     return nums[0]; // this will never happen
+    // }
+
+    // Solution v2.0 - Time complexity: O(n) ~ ms - Memory: mb
     int majorityElement(vector<int> &nums)
     {
         if (nums.size() == 1)
             return nums[0];
-        map<int, int> freq;
-        int count = nums.size() / 2;
+        int res = 0;
+        int count = 0;
         for (int x : nums)
         {
-            freq[x]++;
-            if (freq[x] > count)
-                return x;
+            if (count == 0)
+            {
+                res = x;
+                count++;
+            }
+            else if (x == res)
+                count ++;
+            else
+                count--;
         }
-        return nums[0]; // this will never happen
+        return res;
     }
 };
 
