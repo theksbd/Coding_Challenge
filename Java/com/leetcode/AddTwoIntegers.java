537f1cf463f1b5c21e22557cac87484ff7e0e2c9
Do Thien Hoang <theksbd@gmail.com>
2022-06-18 09:36:43 +0700

    add more solutions

---
+++ Java/com/leetcode/AddTwoIntegers.java
@@ -0,0 +1,14 @@
+//success
+//https://leetcode.com/problems/add-two-integers/
+package com.leetcode;
+
+public class AddTwoIntegers {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        AddTwoIntegers a = new AddTwoIntegers();
+        System.out.println(a.sum(5, 3));
+    }
+}
